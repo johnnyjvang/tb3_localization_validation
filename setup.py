0bc4a3285972064ab6583f23f93bf0c10c5dfe55
jvang <johnnyjvang@gmail.com>
2026-03-23 20:38:46 -0700

    package created

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+package_name = 'tb3_localization_validation'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jvang',
+    maintainer_email='johnnyjvang@gmail.com',
+    description='TODO: Package description',
+    license='MIT',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            # Added to print and reset json output
+            'reset_results = tb3_tf_validation.reset_results:main',
+            'summary_report = tb3_tf_validation.summary_report:main',
+        ],
+    },
+)
